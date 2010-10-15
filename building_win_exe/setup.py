ec6e3c5c45f392529a11518bf586078b48b5e6fd
Kevin Chabowski <kevin@kch42.de>
2010-10-16 00:02:17 +0200

    Added scripts for creating a windows exe file / an installer.

---
+++ building_win_exe/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import py2exe
+setup(name="hometape",
+      version='0.3.7',
+      windows = [
+        {
+            "script": "hometape.py",
+            "icon_resources": [(1, "win_icon.ico")]
+        }
+    ],
+    options = {'py2exe': {'optimize': 2, 'bundle_files': 1, 'compressed': True, \
+                                  'excludes': [], 'packages': ['encodings'], \
+                                  'dll_excludes': ['MSVCP90.dll']} },
+    zipfile = None
+)
